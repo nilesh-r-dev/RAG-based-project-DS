272970af049742981764a05779023fb09c830747
Nilesh <nileshrupnawar911@gmail.com>
2026-05-05 16:30:17 +0530

    Update preprocess_json.py

--- preprocess_json.py
+++ preprocess_json.py
@@ -6,15 +6,29 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import joblib
 
-def create_embedding(text_list):
-    # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
-    r = requests.post("http://localhost:11434/api/embed", json={
+
+
+def create_embedding(text):
+    r = requests.post("http://localhost:11434/api/embeddings", json={
         "model": "bge-m3",
-        "input": text_list
+        "prompt": text   # ✅ correct key
     })
 
-    embedding = r.json()["embeddings"] 
-    return embedding
+    res = r.json()
+    return res.get("embedding", [])
+
+# def create_embedding(text_list):
+#     # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
+#     r = requests.post("http://localhost:11434/api/embed", json={
+#         "model": "bge-m3",
+#         "input": text_list
+#     })
+
+#     #embedding = r.json()["embeddings"] 
+#     res = r.json() 
+#     #embedding = res["embeddings"] if "embeddings" in res else res["embedding"]
+#     embedding = res.get("embeddings") or res.get("embedding") or []
+#     return embedding
 
 
 jsons = os.listdir("jsons")  # List all the jsons 
@@ -25,7 +39,15 @@ for json_file in jsons:
     with open(f"jsons/{json_file}") as f:
         content = json.load(f)
     print(f"Creating Embeddings for {json_file}")
-    embeddings = create_embedding([c['text'] for c in content['chunks']])
+    #embeddings = create_embedding([c['text'] for c in content['chunks']])
+    #embeddings = create_embedding([c['text'] for c in content['chunks']])
+    texts = [c['text'] for c in content['chunks']]
+    #embeddings = [create_embedding(t)[0] for t in texts]
+    embeddings = [create_embedding(t) for t in texts]
+
+    if not embeddings:
+      print(f"Skipping {json_file} due to embedding error")
+      continue
        
     for i, chunk in enumerate(content['chunks']):
         chunk['chunk_id'] = chunk_id
